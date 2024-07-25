ab8601e329e4634ee966d1e57952612a00c857b2
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-07-25 14:01:44 +0900

    feat: create entities

--- src/config/config.typeorm.ts
+++ src/config/config.typeorm.ts
@@ -15,15 +15,20 @@ const typeOrmConfig: TypeOrmModuleOptions = {
   database: process.env.DB_NAME || 'sos_database', 
   entities: [
     path.join(__dirname, '/../**/*.entity{.ts,.js}')
-  ],
-//   entities: [
-//     process.env.NODE_ENV === 'production'
-//       ? path.join(__dirname, '/../**/*.entity.js')
-//       : path.join(__dirname, '/../**/*.entity.ts')
-//   ],  // 엔티티 파일 경로 (NODE_ENV 값에 따라 경로 설정 -> 개발환경에서는 ts, 프로덕션 환경에서는 js)
-  synchronize: true,  // 애플리케이션 실행 시 데이터베이스 스키마를 동기화 (운영 환경에서는 false로 설정)
+  ], 
+  // 방법1: 엔티티 자동 로드
+  // autoLoadEntities: true,
+  // 방법2: 엔티티 경로 설정
+  //   entities: [
+  //     process.env.NODE_ENV === 'production'
+  //       ? path.join(__dirname, '/../**/*.entity.js')
+  //       : path.join(__dirname, '/../**/*.entity.ts')
+  //   ],  // 엔티티 파일 경로 (NODE_ENV 값에 따라 경로 설정 -> 개발환경에서는 ts, 프로덕션 환경에서는 js)
+  synchronize: true,  // 운영 환경에서는 false, 개발 환경에서는 true
   logging: true, 
-  charset: 'utf8mb4',  
+  charset: 'utf8mb4',
+    
+  keepConnectionAlive: true,
 };
 
 // DB 없으면 생성
